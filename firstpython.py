79e7be30e026967052cff718379ce86be9b54c63
Rushi Modi <168322511+Rushi9100@users.noreply.github.com>
2024-05-11 02:26:39 +0100

    Create firstpython.py
    Learned to create a new file in the existing repository.

---
+++ firstpython.py
@@ -0,0 +1,6 @@
+#Display the output
+print("Hello World")
+print("Hello, I'm just trying a create a new file in this same repository")
+
+Coding = "Fun"
+print(f"I am learning quite a lot as it all seems very interesting, I'm really enjoying the coding/ learnig Python. At the end it's too much {Coding}")
