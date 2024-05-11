64acffd08d0e46981fbb92dd2096f0277b1057a9
Rushi Modi <168322511+Rushi9100@users.noreply.github.com>
2024-05-11 04:13:46 +0100

    Create testchild.py
    .py extension in all the files show that the file is created in Python language.

---
+++ testchild.py
@@ -0,0 +1,2 @@
+print("Well, Hellooooowww Ladies and Gentleman. You guys are the real underdogs.")
+print("Sub-branch is basically an extra branch or platform created in the existing repository which does not affect the main branch untill or unless the pull request has been generated and apprved after commiting the changes to the sub-branch. This phenomena could be recognised as a Version control system.")
